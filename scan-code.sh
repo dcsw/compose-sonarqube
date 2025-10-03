57309160bf87901c82b15c7630006d24d4881d68
Dean Cirielli <dcirielli5@gmail.com>
2025-10-03 12:35:44 -0400

    Implement CODE_PATH env var to redirect source directory to scan.

--- scan-code.sh
+++ scan-code.sh
@@ -1,11 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 . ./.token-file
 docker run --rm \
   -e SONAR_HOST_URL="http://host.docker.internal:9000" \
   -e SONAR_TOKEN="$SONAR_TOKEN" \
-  -v ./code:/usr/src \
+  -v ${CODE_PATH:-./code}:/usr/src \
   sonarsource/sonar-scanner-cli \
   -Dsonar.projectKey=$PROJECT_KEY \
   -Dsonar.sources=/usr/src \
   -Dsonar.host.url=http://host.docker.internal:9000 \
-  -Dsonar.token=$SONAR_TOKEN
+  -Dsonar.token=$SONAR_TOKEN \
+  -Dsonar.scm.disabled=true \
+  -Dsonar.inclusions=**/*.js,**/*.ts,**/*.sql
